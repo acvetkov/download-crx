2879de2b87002c8fc6cbbf266e2a28920aba971b
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2017-07-06 12:49:52 +0200

    Show the save file path in the end

--- src/cli.js
+++ src/cli.js
@@ -30,6 +30,10 @@ export default {
     download () {
         const args = parser.parseArgs();
         const savePath = process.cwd();
-        return downloadUtils.download(args.url, savePath, args.name || 'extension');
+
+        return downloadUtils
+          .download(args.url, savePath, args.name || 'extension')
+          .then(filePath => console.log(`Saved in ${filePath}`))
+          .catch(err => conole.log(err))
     }
 };
